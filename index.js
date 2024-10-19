8f62a7b22afe650e13d9826c607acb05073ee451
Paul Donovan <paul.m.donovan@outlook.com>
2024-10-19 11:59:24 +1100

    finishes JS

---
+++ index.js
@@ -0,0 +1,11 @@
+function openNavbar() {
+    document.getElementById("sideNavigationBar")
+        .style.width = "50%";
+}
+function closeNavbar() {
+
+    document.getElementById("sideNavigationBar")
+
+        .style.width = "0%";
+
+} 
